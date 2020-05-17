58e238a069e54321aeae5c209460e616775b045a
Jakub <kubplon@gmail.com>
2020-05-17 08:42:37 +0200

    Initial commit

---
+++ src/pl/sda/Main.java
@@ -0,0 +1,29 @@
+package pl.sda;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Zaszyfruj hasło otrzymane w argumentach aplikacji (args) za pomocą następującego szyfru
+        /*
+            a=l
+            s=k
+            d=j
+            f=h
+            g=g
+            q=p
+            w=o
+            e=i
+            r=u
+            t=y
+            z=m
+            x=n
+            c=b
+            v=v
+            1=0
+            2=9
+            3=8
+            4=7
+            5=6
+         */
+    }
+}
